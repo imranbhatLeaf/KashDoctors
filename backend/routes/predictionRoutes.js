e82dc79f65af336cdef9d07c5b055698d03ade16
imranbhatLeaf <imranbhat9444@gmail.com>
2026-06-11 10:21:53 +0530

    resolve merge conflicts

--- backend/routes/predictionRoutes.js
+++ backend/routes/predictionRoutes.js
@@ -1,9 +1,5 @@
 const express = require('express');
-<<<<<<< HEAD
-const { getPrediction, getSymptoms, chat } = require('../controllers/predictionController');
-=======
 const { getPrediction, getSymptoms } = require('../controllers/predictionController');
->>>>>>> origin/main
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
@@ -11,9 +7,5 @@ const router = express.Router();
 // You can add 'protect' middleware here if you want only registered users to use the AI
 router.post('/', getPrediction);
 router.get('/symptoms', getSymptoms);
-<<<<<<< HEAD
-router.post('/chat', chat);
-=======
->>>>>>> origin/main
 
 module.exports = router;
